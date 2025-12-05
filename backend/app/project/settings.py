204449520485f2e6485d8d743eb45c1f0e0d850d
aizazja028-sketch <aizazja028@gmail.com>
2025-12-06 00:25:38 +0500

    Update settings.py

--- backend/app/project/settings.py
+++ backend/app/project/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config, Csv
-import pymysql
-pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
